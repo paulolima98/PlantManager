4f3e6926f4fffb7543a0e91a14d7b4f716bed843
Paulo <pauloh_gl@hotmail.com>
2022-07-31 16:53:10 -0300

    Aula 2 - break point
    Parte das telas criadas, pronto pra implementar uma navegação entre elas.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,43 @@
-import React from "react";
-import { Welcome } from "./src/pages/Welcome";
-
+import React, { useEffect, useCallback } from "react";
+import { SafeAreaView } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import { 
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from "@expo-google-fonts/jost";
+import { Confirmation } from "./src/pages/Confirmation";
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Welcome />
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Confirmation />
+    </SafeAreaView>
   );
 }
